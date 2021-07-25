ed66a68ee814d60ff470bc6f6d1a97f8b9d503a4
Fernando Stockler de Lima <fernandostockler@uol.com.br>
2021-07-25 05:25:50 -0300

    ajuste

--- UniversalWindowsStart/MVVM/ViewModelBase.cs
+++ UniversalWindowsStart/MVVM/ViewModelBase.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UniversalWindowsStart.MVVM
+﻿namespace UniversalWindowsStart.MVVM
 {
     public abstract class ViewModelBase : NotifyPropertyChangedBase
     {

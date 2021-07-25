3f5c1b8c3ce11a93528072f0f0a1685adb115bec
Fernando Stockler de Lima <fernandostockler@uol.com.br>
2021-07-25 05:11:27 -0300

    MVVM -> ViewModelBase class

--- UniversalWindowsStart/MVVM/NotifyPropertyChangedBase.cs
+++ UniversalWindowsStart/MVVM/NotifyPropertyChangedBase.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UniversalWindowsStart.MVVM
 {
@@ -12,7 +8,7 @@ namespace UniversalWindowsStart.MVVM
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void NotifyPropertyChanged([CallerMemberName]string propertyName = null)
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
---
+++ UniversalWindowsStart/MVVM/ViewModelBase.cs
@@ -0,0 +1,12 @@
+﻿namespace UniversalWindowsStart.MVVM
+{
+    public abstract class ViewModelBase : NotifyPropertyChangedBase
+    {
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+    }
+}

6c97f21133d9401b2fb15661e836671fb52e6a27
Fernando Stockler de Lima <fernandostockler@uol.com.br>
2021-07-25 05:11:27 -0300

    MVVM -> ViewModelBase class

---
+++ UniversalWindowsStart/MVVM/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalWindowsStart.MVVM
+{
+    public abstract class ViewModelBase : NotifyPropertyChangedBase
+    {
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+    }
+}
